cdd11eddcc4a09227c9704e8e88a21fe58e6bb78
Nandakumar MV <nandu2255n@gmail.com>
2023-12-15 22:06:52 +0530

    links

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,11 +1,18 @@
-export const getCoursesUrl = "http://localhost:5000/api/tutor/get-courses";
-export const getAllCouresesUrl = "http://localhost:5000/api/admin/get-courses";
+export const getCoursesUrl =
+  "https://skillify-backend.nandakumarmv.co/api/tutor/get-courses";
+export const getAllCouresesUrl =
+  "https://skillify-backend.nandakumarmv.co/api/admin/get-courses";
 export const bannerVideo =
   "https://v1.pinimg.com/videos/mc/720p/f2/a5/e5/f2a5e557bb57622e0945c1cb9511d7f7.mp4";
 export const getApprovedAllCouresesUrl =
-  "http://localhost:5000/api/get-approvedCourses";
-export const getalldoamins = "http://localhost:5000/api/admin/domains";
-export const createOrderUrl = "http://localhost:5000/api/create-order";
-export const getMyCoursesUrl = "http://localhost:5000/api/my-courses";
-export const getWishlistUrl = "http://localhost:5000/api/get-wishlist";
-export const getSuggestionUrl = "http://localhost:5000/api/get-suggestions";
+  "https://skillify-backend.nandakumarmv.co/api/get-approvedCourses";
+export const getalldoamins =
+  "https://skillify-backend.nandakumarmv.co/api/admin/domains";
+export const createOrderUrl =
+  "https://skillify-backend.nandakumarmv.co/api/create-order";
+export const getMyCoursesUrl =
+  "https://skillify-backend.nandakumarmv.co/api/my-courses";
+export const getWishlistUrl =
+  "https://skillify-backend.nandakumarmv.co/api/get-wishlist";
+export const getSuggestionUrl =
+  "https://skillify-backend.nandakumarmv.co/api/get-suggestions";
